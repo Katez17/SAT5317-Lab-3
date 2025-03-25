ddf92c7793745b653f5063cc582eb618ff5fd7fd
Katez17 <katez@mtu.edu>
2025-03-25 10:50:29 -0400

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:sat5317_lab3/connect_device.dart';
-import 'package:sat5317_lab3/daily_checkin.dart';
-import 'package:sat5317_lab3/home_page.dart';
-import 'package:sat5317_lab3/stress_overview.dart';
+import 'package:project5317/api_functions.dart';
+import 'package:project5317/connect_device.dart';
+import 'package:project5317/daily_checkin.dart';
+import 'package:project5317/home_page.dart';
+import 'package:project5317/stress_overview.dart';
 
 void main() {
   testWidgets('HomePage displays buttons and navigates to screens', (WidgetTester tester) async {
